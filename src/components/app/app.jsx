4d9455bab2606718dd953f2ceb128510245a4262
Manimall <gorpunov94@mail.ru>
2019-05-07 09:03:03 +0300

    test

---
+++ src/components/app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {WelcomeScreen} from '../welcome-screen/welcome-screen';
+
+export const App = (props) => {
+  const {gameTime, errorCount} = props;
+
+  return (
+    <WelcomeScreen
+      time={gameTime}
+      errorCount={errorCount}
+    />
+  );
+};
